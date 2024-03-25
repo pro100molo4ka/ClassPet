7b68ba0798d5c4f6d3b96ec37a76b6017943d6ac
pro100molo4ka <145564751+pro100molo4ka@users.noreply.github.com>
2024-03-25 17:53:55 +0300

    Add files via upload

---
+++ polimorthysmShape4.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void draw() = 0;
+    virtual double CalculationArea() = 0;
+};
+
+class Circle : public Shape {
+private:
+    double radius;
+
+public:
+    Circle(double radius) : radius(radius) {}
+
+    void draw() override {
+       cout << "Рисуем круг радиусом " << radius << endl;
+    }
+
+    double CalculationArea() override {
+        return 3.14 * radius * radius;
+    }
+};
+
+class Cylinder : public Shape {
+private:
+    double radius;
+    double height;
+
+public:
+    Cylinder(double radius, double height) : radius(radius), height(height) {}
+
+    void draw() override {
+        cout << "Рисуем цилиндр радиусом " << radius << " и высотой " << height << endl;
+    }
+
+    double CalculationArea() override {
+        return 2 * 3.14 * radius * radius + 2 * 3.14 * radius * height;
+    }
+};
+
+int main() {
+
+    setlocale(LC_ALL, "RU"); 
+    system("chcp 1251");
+
+    Shape* shapes[] = { new Circle(5), new Cylinder(5, 10) };
+    for (Shape* shape : shapes) {
+        shape->draw();
+       cout << "Площадь фигуры: " << shape->CalculationArea() << endl;
+        delete shape;
+    }
+    return 0;
+}
---
+++ polimorthysmVehicle2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+class Vehicle {
+public:
+    virtual void startEngine() = 0;
+    virtual void stopEngine() = 0;
+};
+
+class Car : public Vehicle {
+public:
+    void startEngine() override {
+        cout << "Запускаем двигатель автомобиля" << endl;
+    }
+
+    void stopEngine() override {
+        cout << "Останавливаем двигатель автомобиля" << endl;
+    }
+};
+
+class Motorcycle : public Vehicle {
+public:
+    void startEngine() override {
+        cout << "Запускаем двигатель мотоцикла" << endl;
+    }
+
+    void stopEngine() override {
+        cout << "Останавливаем двигатель мотоцикла" << endl;
+    }
+};
+
+int main() {
+
+    setlocale(LC_ALL, "RU"); 
+    system("chcp 1251");
+
+    Vehicle* vehicles[] = { new Car, new Motorcycle };
+    for (Vehicle* vehicle : vehicles) {
+        vehicle->startEngine();
+        vehicle->stopEngine();
+        delete vehicle;
+    }
+    return 0;
+}
