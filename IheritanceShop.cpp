33165558e5aa0a6d7103e729706d6b9b2e5b0d09
pro100molo4ka <145564751+pro100molo4ka@users.noreply.github.com>
2024-03-25 23:40:27 +0300

    Add files via upload

---
+++ IheritanceShop.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Product {
+private:
+    string name;
+    double price;
+    int rating;
+
+public:
+    Product() : name(""), price(0), rating(0) {}
+    Product(string name, double price, int rating) : name(name), price(price), rating(rating) {}
+
+    string getName() {
+        return name;
+    }
+
+    double getPrice() {
+        return price;
+    }
+
+    int getRating() {
+        return rating;
+    }
+};
+
+class Category {
+private:
+   string name;
+    Product products[10];
+    int count;
+
+public:
+    Category(string name) : name(name), count(0) {}
+
+    void addProduct(Product product) {
+        products[count] = product;
+        count++;
+    }
+
+    void displayProducts() {
+        for (int i = 0; i < count; i++) {
+            cout << "Название: " << products[i].getName() << ", Цена: " << products[i].getPrice() << ", Рейтинг: " << products[i].getRating() << endl;
+        }
+    }
+};
+
+class Basket {
+private:
+    Product products[10];
+    int count;
+
+public:
+    Basket() : count(0) {}
+
+    void addProduct(Product product) {
+        products[count] = product;
+        count++;
+    }
+
+    void displayProducts() {
+        for (int i = 0; i < count; i++) {
+            cout << "Название: " << products[i].getName() << ", Цена: " << products[i].getPrice() << ", Рейтинг: " << products[i].getRating() << endl;
+        }
+    }
+};
+
+class User {
+private:
+    string login;
+   string password;
+    Basket basket;
+
+public:
+    User(string login, string password) : login(login), password(password) {}
+
+    void addProductToBasket(Product product) {
+        basket.addProduct(product);
+    }
+
+    void displayBasketProducts() {
+        basket.displayProducts();
+    }
+};
+
+int main() {
+
+    setlocale(LC_ALL, "RU");
+    system("chcp 1251");
+
+    Category category1("Электроника");
+    Category category2("Книги");
+    Category category3("Игрушки");
+
+    Product product1("Телевизор", 10000, 4);
+    Product product2("Книга", 500, 3);
+    Product product3("Игрушка", 1000, 2);
+
+    category1.addProduct(product1);
+    category2.addProduct(product2);
+    category3.addProduct(product3);
+
+    category1.displayProducts();
+    category2.displayProducts();
+    category3.displayProducts();
+
+    User user1("Иван", "12345");
+    User user2("Петр", "67890");
+
+    user1.addProductToBasket(product1);
+    user1.addProductToBasket(product2);
+    user2.addProductToBasket(product3);
+
+    user1.displayBasketProducts();
+    user2.displayBasketProducts();
+
+    return 0;
+}
---
+++ Readers.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Reader {
+private:
+   string firstName;
+    string lastName;
+    int readerTicketNumber;
+    string faculty;
+    string birthDate;
+    string phoneNumber;
+
+public:
+    Reader(string firstName, string lastName, int readerTicketNumber, string faculty, string birthDate, string phoneNumber) : firstName(firstName), lastName(lastName), readerTicketNumber(readerTicketNumber), faculty(faculty), birthDate(birthDate), phoneNumber(phoneNumber) {}
+
+    void takeBook() {
+        cout << "Читатель " << firstName << " " << lastName << " взял книгу" << endl;
+    }
+
+    void returnBook() {
+        cout << "Читатель " << firstName << " " << lastName << " вернул книгу" << endl;
+    }
+
+    string getFirstName() {
+        return firstName;
+    }
+
+    string getLastName() {
+        return lastName;
+    }
+
+    int getReaderTicketNumber() {
+        return readerTicketNumber;
+    }
+
+    string getFaculty() {
+        return faculty;
+    }
+
+    string getBirthDate() {
+        return birthDate;
+    }
+
+    string getPhoneNumber() {
+        return phoneNumber;
+    }
+};
+
+class Book {
+private:
+   string author;
+    string title;
+    int year;
+
+public:
+    Book(string author, string title, int year) : author(author), title(title), year(year) {}
+
+    string getAuthor() {
+        return author;
+    }
+
+    string getTitle() {
+        return title;
+    }
+
+    int getYear() {
+        return year;
+    }
+};
+
+int main() {
+
+    setlocale(LC_ALL, "RU"); 
+    system("chcp 1251");
+
+    Reader reader1("Иван", "Иванов", 1234, "Факультет математики", "01-01-2001", "+79123456789");
+    Reader reader2("Петр", "Петров", 5678, "Факультет физики", "02-02-2002", "+79234567890");
+    Book book1("Автор1", "Название1", 2022);
+    Book book2("Автор2", "Название2", 2023);
+
+    reader1.takeBook();
+    reader2.takeBook();
+    reader1.returnBook();
+    reader2.returnBook();
+
+    return 0;
+}
